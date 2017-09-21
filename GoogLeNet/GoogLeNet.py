d24c341035bb26d800e9b315616700688f8d1ff1
Minghao Wu <minghaowu_2015@163.com>
2017-09-21 22:17:19 +0800

    Update GoogLeNet.py

--- GoogLeNet/GoogLeNet.py
+++ GoogLeNet/GoogLeNet.py
@@ -29,7 +29,7 @@ class Inception(nn.Module):
         )
         self.b4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_channels=in_channels, kernel_size=1, padding=1),
+            nn.Conv2d(in_channels=in_channels, out_channels=pool_proj, kernel_size=1, padding=1),
             nn.ReLU(inplace=True)
         )
     def forward(self, x):
@@ -53,14 +53,14 @@ class GoogLeNet(nn.Module):
         self.block3a = Inception(in_channels=192, k_1x1=64, k_3x3red=96, k_3x3=128, k_5x5red=16, k_5x5=32, pool_proj=32)
         self.block3b = Inception(in_channels=256, k_1x1=128, k_3x3red=128, k_3x3=192, k_5x5red=32, k_5x5=96, pool_proj=64)
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.block4a = Inception(in_channels=512, k_1x1=192, k_3x3red=96, k_3x3=208, k_5x5red=16, k_5x5=48, pool_proj=64)
+        self.block4a = Inception(in_channels=480, k_1x1=192, k_3x3red=96, k_3x3=208, k_5x5red=16, k_5x5=48, pool_proj=64)
         self.block4b = Inception(in_channels=512, k_1x1=160, k_3x3red=112, k_3x3=224, k_5x5red=24, k_5x5=64, pool_proj=64)
         self.block4c = Inception(in_channels=512, k_1x1=128, k_3x3red=128, k_3x3=256, k_5x5red=24, k_5x5=64, pool_proj=64)
-        self.block4d = Inception(in_channels=528, k_1x1=112, k_3x3red=144, k_3x3=288, k_5x5red=32, k_5x5=64, pool_proj=64)
-        self.block4e = Inception(in_channels=832, k_1x1=256, k_3x3red=160, k_3x3=320, k_5x5red=32, k_5x5=128, pool_proj=128)
+        self.block4d = Inception(in_channels=512, k_1x1=112, k_3x3red=144, k_3x3=288, k_5x5red=32, k_5x5=64, pool_proj=64)
+        self.block4e = Inception(in_channels=528, k_1x1=256, k_3x3red=160, k_3x3=320, k_5x5red=32, k_5x5=128, pool_proj=128)
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.block5a = Inception(in_channels=832, k_1x1=256, k_3x3red=160, k_3x3=320, k_5x5red=32, k_5x5=128, pool_proj=128)
-        self.block5b = Inception(in_channels=1024, k_1x1=384, k_3x3red=192, k_3x3=384, k_5x5red=48, k_5x5=128, pool_proj=128)
+        self.block5b = Inception(in_channels=832, k_1x1=384, k_3x3red=192, k_3x3=384, k_5x5red=48, k_5x5=128, pool_proj=128)
         self.pool3 = nn.AvgPool2d(kernel_size=7, stride=1)
         self.drop = nn.Dropout(p=0.4)
         self.classifier = nn.Linear(in_features=1024, out_features=num_classes)
@@ -82,3 +82,17 @@ class GoogLeNet(nn.Module):
         x = self.drop(x)
         x = self.classifier(x)
         return(x)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
